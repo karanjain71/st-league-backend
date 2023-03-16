f004b6e4cca27f155e0c5a974be1cdf98fd50fa6
I539328 <karan.jain@sap.com>
2023-03-16 20:08:10 +0530

    First commit

---
+++ src/main/java/com/projects/stockleague/StockLeagueApplication.java
@@ -0,0 +1,14 @@
+package com.projects.stockleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockLeagueApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
---
+++ src/test/java/com/projects/stockleague/StockLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.stockleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
