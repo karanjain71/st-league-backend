71dd4908ded36bde3af3c0bf715097216f11cc9b
Karan Jain <karan.jain@sap.com>
2023-04-16 14:28:36 +0530

    Added changes

--- src/main/java/com/stocks/controllers/AuthController.java
+++ src/main/java/com/stocks/controllers/AuthController.java
@@ -6,11 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.stocks.payloads.JwtAuthRequest;
 import com.stocks.payloads.JwtAuthResponse;
-import com.stocks.repositories.UserRepository;
 import com.stocks.security.JwtTokenHelper;
 
 @RestController
@@ -32,35 +28,10 @@ public class AuthController {
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
-//	@PostMapping("/register")
-//	public ResponseEntity<JwtAuthResponse> registerUser(@RequestBody JwtAuthRequest request) throws Exception{
-//		
-//		System.out.println(request.getUsername() + " " + request.getPassword());
-////		request.setPassword(request.getPassword())
-//		Authentication authentication = authenticationManager.authenticate(
-//                new UsernamePasswordAuthenticationToken(
-//                request.getUsername(),
-//                request.getPassword()));
-//        SecurityContextHolder.getContext().setAuthentication(authentication);
-//		
-//        String token = this.jwtTokenHelper.generateToken(authentication);
-//
-//		JwtAuthResponse response = new JwtAuthResponse();
-//		response.setToken(token);
-//		
-//		return new ResponseEntity<>(response, HttpStatus.OK);
-//	}
-	
 	@PostMapping("/login")
 	public ResponseEntity<JwtAuthResponse> createToken(@RequestBody JwtAuthRequest request) throws Exception{
 		
 		this.authenticate(request.getUsername(), request.getPassword());
-		System.out.println(request.getUsername() + " " + request.getPassword());
-		Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                request.getUsername(),
-                request.getPassword()));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
 		
 		UserDetails userDetails = this.userDetailsService.loadUserByUsername(request.getUsername());
 		
--- src/main/java/com/stocks/security/JwtTokenHelper.java
+++ src/main/java/com/stocks/security/JwtTokenHelper.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
